6f9bf9579d6e252093fed0c4e92202d280c7027f
Travis Beatty <travisby@gmail.com>
2013-08-10 11:27:22 -0400

    Fixed our return type for post request

--- secret_truth.py
+++ secret_truth.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/secret', methods=['GET', 'POST'])
 def get_secret():
-    return 'secret'
+    return '', 201
